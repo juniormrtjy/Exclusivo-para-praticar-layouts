a5d018485acc689f47401ac6ec39b89c5070d2bf
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-01-07 10:56:03 -0300

    finishing nav and starting the header

--- 01/main.js
+++ 01/main.js
@@ -1,3 +1,11 @@
+function onScroll() {
+  if (scrollY > 0) {
+    nav.classList.add('scroll')
+  } else {
+    nav.classList.remove('scroll')
+  }
+}
+
 function toggleMenu() {
   document.body.classList.toggle('menu-expanded')
 }
