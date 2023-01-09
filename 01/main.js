b65d28c14491c612aae41f236b65f2c535a0701c
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-01-09 19:01:16 -0300

    praticando

--- 01/main.js
+++ 01/main.js
@@ -9,3 +9,15 @@ function onScroll() {
 function toggleMenu() {
   document.body.classList.toggle('menu-expanded')
 }
+
+// scrollreveal
+
+ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 1000
+}).reveal(`
+  #home, #home img, #home .stats,
+  #services, #services .card, #services .cards,
+  #about, #about p, #about img
+`)
